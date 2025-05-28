d2df67d58471f2c1119a734da026a29ccaebc72d
HSD06040 <tinto0604@gmail.com>
2025-05-28 10:46:08 +0900

    [Feat] NoteData
    롱 노트를 고려하여 필요한 노트의 데이터를 제작하였다.

---
+++ Assets/Scripts/Note/NoteData.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MusicData
+{
+    public string musicName;
+    public string artistName;
+    public int BPM;
+}
+
+[Serializable]
+public class MapData
+{
+    public MusicData musicData;
+    public List<NoteData> notes;
+}
+
+[Serializable]
+public class NoteData
+{
+    public void TransmitData(NoteData _to)
+    {
+        _to.keyPos = this.keyPos;
+        _to.startTime = this.startTime;
+        _to.endTime = this.endTime;
+        _to.noteType = this.noteType;
+        _to.keySound = this.keySound;
+        _to.keySoundVolume = this.keySoundVolume;
+        _to.longNoteData = this.longNoteData;
+
+        _to.isLongNoteBody = this.isLongNoteBody;
+        _to.isLongNoteEnd = this.isLongNoteEnd;
+        _to.isLongNoteStart = this.isLongNoteStart;
+    }
+
+    public int keyPos;
+    public int noteType;
+    public NoteData longNoteData;
+    public float startTime;
+    public float endTime;
+    public string keySound;
+    public float keySoundVolume;
+    public bool isJudgeDone;
+
+    public bool isLongNoteStart;
+    public bool isLongNoteBody;
+    public bool isLongNoteEnd;
+}
